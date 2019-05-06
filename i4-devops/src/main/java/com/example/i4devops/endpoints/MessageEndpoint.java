9a093dca6e7cf639671b17e7598504188c89498a
BartvaiErika <erika.bartvai@gmail.com>
2019-05-06 15:52:41 +0200

    Break tests

--- i4-devops/src/main/java/com/example/i4devops/endpoints/MessageEndpoint.java
+++ i4-devops/src/main/java/com/example/i4devops/endpoints/MessageEndpoint.java
@@ -10,7 +10,7 @@ public class MessageEndpoint {
 
   @GetMapping
   String get() {
-    return "Learning DevOps today and second time.";
+    return "Learning DevOps today and second time.!!";
   }
 
 }
